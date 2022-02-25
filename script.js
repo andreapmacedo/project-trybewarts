b52dec31ae328b8b31bd16b6589f8a967ef4230b
Andre <aa.andremacedo@gmail.com>
2022-02-25 19:26:03 -0300

    modifica o desafio 18

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const btnLogin = document.querySelector('#loginBtn');
-const btnSubmit = document.querySelector('#submit-btn');
+const btnSubmit = document.querySelector('#submit-btn')
 const checkAcordo = document.querySelector('#agreement');
+
+
+btnSubmit.disabled = true;
 
 function login() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -18,6 +21,20 @@ function submit(evento) {
     evento.preventDefault();
   }
 }
+
+function toAble(event){
+  // let e = event.target;
+  if(checkAcordo.value !== 'on'){
+    btnSubmit.disabled = true;
+  } 
+  if (checkAcordo.value === 'on'){
+    btnSubmit.disabled = false;
+  }
+
+}
+
+checkAcordo.addEventListener('click', toAble)
+
 window.onload = function carrega(evento) {
   evento.preventDefault();
   btnSubmit.addEventListener('click', submit);
