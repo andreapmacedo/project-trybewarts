1323210e9d81308a0577f22392255cb49a4423c7
Andre <aa.andremacedo@gmail.com>
2022-02-25 16:59:46 -0300

    adiciona o desafio 4

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const email = document.querySelector(".email");
-const senha = document.querySelector(".senha");
-const btnLogin = document.querySelector(".btnLogin");
-// const btnEmail = document.querySelector(".btnLogin");
+const email = document.querySelector("#email");
+const senha = document.querySelector("#senha");
+const btnLogin = document.querySelector("#loginBtn");
 
 function login() {
+    console.log(email.value)
   if(email.value === 'tryber@teste.com' && senha.value === '123456'){
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-
 }
 btnLogin.addEventListener('click', login)
