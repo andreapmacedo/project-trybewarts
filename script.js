ff12a9113efabca8a4b7fc6689ebee92172b19a6
Andre <aa.andremacedo@gmail.com>
2022-02-25 16:39:08 -0300

    adiciona os desafios 5 6 e 7

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const email = document.querySelector(".email");
 const senha = document.querySelector(".senha");
 const btnLogin = document.querySelector(".btnLogin");
+// const btnEmail = document.querySelector(".btnLogin");
 
 function login() {
-  
+  if(email.value === 'tryber@teste.com' && senha.value === '123456'){
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+
 }
+btnLogin.addEventListener('click', login)
